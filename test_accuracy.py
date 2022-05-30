c5b802f4007e49dfe86561d93599200a7c1263d4
gh-copilot <96784752+gh-copilot@users.noreply.github.com>
2022-05-30 22:37:48 +0900

    Update test_accuracy.py

--- test_accuracy.py
+++ test_accuracy.py
@@ -8,9 +8,11 @@ if (__name__ == '__main__' and __package__ is None) or __package__ == '':
     __package__ = '.'.join(file.parent.parts[len(file.parents[3].parts):])
 
 import sys
-from IPython.core import ultratb
-sys.excepthook = ultratb.FormattedTB(mode='Verbose', color_scheme='Linux', call_pdb=False)
-
+try:
+    from IPython.core import ultratb
+    sys.excepthook = ultratb.FormattedTB(mode='Verbose', color_scheme='Linux', call_pdb=False)
+except:
+    pass
 import os
 from feature_extracton import FeatureExtraction
 import knn
