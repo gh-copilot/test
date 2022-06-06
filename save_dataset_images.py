ea34546040d26768b152e446916f426b381b8bab
gh-copilot <96784752+gh-copilot@users.noreply.github.com>
2022-06-06 09:52:19 +0900

    updated save_dataset_images [skip ci]

--- save_dataset_images.py
+++ save_dataset_images.py
@@ -10,32 +10,36 @@ from datetime import datetime
 
 face_detector = dlib.get_frontal_face_detector()
 
+np.random.seed(42)
+SELECTED_SIZE = 45
 
-def get_files_count(directory):
-    return int(
-        subprocess.check_output(f"find '{directory}' -type f | wc -l", shell=True, stderr=subprocess.STDOUT, text=True))
 
-def load_dataset(path_to_dataset):
+def load_dataset(path_to_dataset, output_path, numpy_output_path):
     path_to_dataset = os.path.join(os.getcwd(), path_to_dataset)
+    
+    files = list(os.listdir(path_to_dataset))
+    random_files = np.random.choice(files, size=SELECTED_SIZE, replace=False)
+    print(f'Dataset Size: {len(files)}')
 
-    dataset_size = get_files_count(path_to_dataset)
-    print(f'Dataset Size: {dataset_size}')
-    for root, dirs, files in os.walk(path_to_dataset):
-        for file in files:
-            if file.lower().endswith(".avi"):
-                continue
-            path = os.path.join(root, file)
-            try:
-                img = cv2.imread(path)
-                face = face_detector(img, 0)[0]
-                img = img[face.top():face.bottom(), face.left():face.right()]
-                cv2.imwrite(path, img)
-            except Exception as e:
-                print(f"ERROR at '{path}'", e)
+    for file in files:
+        path = os.path.join(path_to_dataset, file)
+        try:
+            img = cv2.imread(path)
+            face = face_detector(img, 0)[0]
+            img = img[face.top():face.bottom(), face.left():face.right()]
+            cv2.imwrite(os.path.join(output_path, file), img)
+            
+            if file in random_files:
+                img = cv2.resize(img, (48, 48)).ravel()
+                np.save(os.path.join(numpy_output_path, file), img)
+
+        except Exception as e:
+            print(f"ERROR at '{path}'", e)
+        
     print('Finished saving dataset.')
 
 
 start = datetime.now()
 print(f"Loading dataset {sys.argv[1]}")
-load_dataset(sys.argv[1])
+load_dataset(sys.argv[1], sys.argv[2], sys.argv[3])
 print(f"Time Taken: {datetime.now() - start}")
