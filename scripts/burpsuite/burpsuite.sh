6a1cf19e897cd7b1ef38295db3b5ace5461343ea
gh-copilot <96784752+gh-copilot@users.noreply.github.com>
2022-08-18 00:42:56 +0200

    Update burpsuite.sh

--- scripts/burpsuite/burpsuite.sh
+++ scripts/burpsuite/burpsuite.sh
@@ -6,7 +6,7 @@ rm .SRCINFO
 
 sed -i "s/(free edition)/(minimal free edition)/g" PKGBUILD
 sed -i 's/noextract=("${pkgname}-${pkgver}.jar")/noextract=("${pkgname}-${pkgver}.sh")/g' PKGBUILD
-sed -i "s/&type=Jar/&type=Linux/g" PKGBUILD
+sed -i "s/type=Jar/type=Linux/g" PKGBUILD
 sed -i 's/source=("${pkgname}-${pkgver}.jar::/source=("${pkgname}-${pkgver}.sh::/g' PKGBUILD
 sed -i -E "s/sha256sums=\(.*/sha256sums=\('SKIP'/g" PKGBUILD
 
