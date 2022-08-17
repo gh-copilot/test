98674297115ff5b1d1dcb947c3c515a7c425e1ab
gh-copilot <96784752+gh-copilot@users.noreply.github.com>
2022-08-17 23:32:23 +0200

    burpsuite [skip ci]

---
+++ scripts/burpsuite/burpsuite.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname -- "$0")
+
+rm .SRCINFO
+
+sed -i "s/(free edition)/(minimal free edition)/g" PKGBUILD
+sed -i "/^noextract/d" PKGBUILD
+sed -i "s/&type=Jar/&type=Linux/g" PKGBUILD
+sed -i 's/source=("${pkgname}-${pkgver}.jar::/source=("${pkgname}-${pkgver}.sh::/g' PKGBUILD
+sed -i -E "s/sha256sums=\(.*/sha256sums=\('SKIP'/g" PKGBUILD
+
+git apply $SCRIPT_DIR/burpsuite.patch
+
+cat PKGBUILD
+
